5d9736e4da5f7c35993bbd8f59e29c953e1544fa
Jardson Alves <jardson123442@gmail.com>
2024-04-19 15:07:55 -0300

    Possible moves

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -2,16 +2,16 @@ package boardgame;
 
 public class Board {
 
-    private  int rows;
-    private  int columns;
+    private int rows;
+    private int columns;
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
         if (rows < 1 || columns < 1) {
             throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
         }
-        this.columns = columns;
         this.rows = rows;
+        this.columns = columns;
         pieces = new Piece[rows][columns];
     }
 
@@ -19,17 +19,18 @@ public class Board {
         return rows;
     }
 
-    public int getColumn() {
+    public int getColumns() {
         return columns;
     }
 
-    public Piece piece(int row, int column){
+    public Piece piece(int row, int column) {
         if (!positionExists(row, column)) {
             throw new BoardException("Position not on the board");
         }
         return pieces[row][column];
     }
-    public Piece piece(Position position){
+
+    public Piece piece(Position position) {
         if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
         }
@@ -60,6 +61,7 @@ public class Board {
     private boolean positionExists(int row, int column) {
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
+
     public boolean positionExists(Position position) {
         return positionExists(position.getRow(), position.getColumn());
     }
